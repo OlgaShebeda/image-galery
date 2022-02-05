6e148631b2e35ab24c43c4f70084b22abbd9777a
Olga kucheiko <kucheikoolga@gmail.com>
2022-02-05 13:56:05 +0300

    fix:fixing bagges

--- player/index.js
+++ player/index.js
@@ -4,17 +4,25 @@ const btn = document.querySelector('.btn-play');
 const navList = document.querySelectorAll(".nav-item");
 const bg = document.querySelector('.bg-img');
 const audio = document.querySelector("audio");
+let isPlay = false;
 
 function activeItem(){
     navList.forEach(item => {
       item.classList.remove('active');
       event.target.classList.add('active');
     })
+
+    if(isPlay){
+      btn.classList.remove('pause');
+      audio.pause();
+      isPlay = false;
+    }
+    
   }
 
 function activeImg (n){
     let str = "url('./assets/img/" + birds[n] + ".jpg')"
-    bg.style.backgroundImage =str;
+    bg.style.backgroundImage = str;
 }
 
 function activeAudio (n){
@@ -29,13 +37,17 @@ function activeAudio (n){
     })
   });
 
-btn.addEventListener("click", () => {
-    if (btn.classList.contains('pause')){
+function toggleBtn(){
+   if (btn.classList.contains('pause')){
         btn.classList.remove('pause');
         audio.pause();
+        isPlay = false;
     }else{
         btn.classList.add('pause');
         audio.currentTime = 0;
         audio.play();
+        isPlay = true;
     }
-})
+}
+
+btn.addEventListener("click", toggleBtn);
