eba4ab75ea5e6b1b8dcfed87f1cc5a28e793921b
Olga kucheiko <kucheikoolga@gmail.com>
2022-02-22 21:15:59 +0300

    feat: add window won

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,6 +1,9 @@
 const area = document.querySelectorAll('.box');
 let index = 0;
 let result = '';
+const reset = document.querySelector('.btn');
+const content = document.querySelector('.content');
+const block = document.querySelector('.win-container');
 
 const win = [
     [0, 1, 2],
@@ -35,19 +38,32 @@ function stepByStep (){
     winCheck(index);
     
     if(index === 9 ){
-      console.log("ничья");
+      block.style.display = 'block';
+      content.innerHTML = "draw";
     } 
 }
 
 function winCheck(ind){
     for (let i = 0; i < win.length; i++){
       if (area[win[i][0]].innerHTML === 'O' && area[win[i][1]].innerHTML === 'O' && area[win[i][2]].innerHTML === 'O'){
-        console.log("won O in " + ind + " step");
+        block.style.display = 'block';
+        content.innerHTML = `won O in ${ind} step`;
+        console.log("0")
       }else{
         if (area[win[i][0]].innerHTML === 'X' && area[win[i][1]].innerHTML === 'X' && area[win[i][2]].innerHTML === 'X'){
-          console.log("won X in " + ind + " step");
+          block.style.display = 'block';
+          content.innerHTML = `won X in ${ind} step`;
+          console.log("x")
         }
       }
     }
 }
 
+reset.addEventListener("click", function() {
+  block.style.display = 'none';
+  location.reload();
+  dataO = [];
+  dataX = [];
+  
+  index = 0;
+});
